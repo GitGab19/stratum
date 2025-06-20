ea05cf6573fb050a3fdf0dc7a13d8c9acba12dc8
bit-aloo <sshourya17@gmail.com>
2025-06-20 20:20:40 +0530

    uncomplete bootstrap flow

--- roles/new-tproxy/src/lib/proxy/channel_manager.rs
+++ roles/new-tproxy/src/lib/proxy/channel_manager.rs
@@ -1,12 +1,20 @@
-use crate::{downstream_sv1::downstream::Downstream, error::Error, upstream_sv2::upstream::StdFrame};
+use crate::{
+    downstream_sv1::downstream::Downstream, error::Error, upstream_sv2::upstream::StdFrame,
+};
 use async_channel::{Receiver, Sender};
 use binary_sv2::u256_from_int;
-use roles_logic_sv2::{channels::client::extended::ExtendedChannel, parsers::{Mining, AnyMessage}, utils::Mutex, mining_sv2::OpenExtendedMiningChannel, handlers::mining::ParseMiningMessagesFromUpstream};
-use tracing::error;
+use roles_logic_sv2::{
+    channels::client::extended::ExtendedChannel,
+    handlers::mining::{ParseMiningMessagesFromUpstream, SendTo},
+    mining_sv2::OpenExtendedMiningChannel,
+    parsers::{AnyMessage, Mining},
+    utils::Mutex,
+};
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock},
 };
+use tracing::error;
 
 pub type Sv2Message = Mining<'static>;
 
@@ -28,8 +36,11 @@ pub struct ChannelManager {
     upstream_receiver: Receiver<Mining<'static>>,
     // This is a mapping of the channel id to the extended channel.
     pub extended_channels: HashMap<u32, Arc<RwLock<ExtendedChannel<'static>>>>,
-    /*// This is a mapping of the downstream id to the downstream.
-    pub downstreams: HashMap<u32, Arc<Mutex<Downstream>>>,*/
+
+    sv1_server_sender: Sender<Mining<'static>>,
+
+    channel_opener_receiver: Receiver<(u32, String)>, /*// This is a mapping of the downstream id to the downstream.
+                                                      pub downstreams: HashMap<u32, Arc<Mutex<Downstream>>>,*/
 }
 
 impl ChannelManager {
@@ -37,45 +48,62 @@ impl ChannelManager {
         // mode: ChannelMappingMode,
         upstream_sender: Sender<Mining<'static>>,
         upstream_receiver: Receiver<Mining<'static>>,
+        sv1_server_sender: Sender<Mining<'static>>,
+        channel_opener_receiver: Receiver<(u32, String)>,
     ) -> Self {
         Self {
             // mode,
             upstream_sender,
             upstream_receiver,
             extended_channels: HashMap::new(),
-            //downstreams: HashMap::new(),
+            sv1_server_sender,
+            channel_opener_receiver, //downstreams: HashMap::new(),
         }
     }
 
-    pub async fn on_upstream_message(&mut self) -> Result<(), Error> {
+    pub async fn on_upstream_message(&self) -> Result<(), Error> {
         while let Ok(message) = self.upstream_receiver.recv().await {
-            let mut frame: StdFrame =
-                AnyMessage::Mining(message).try_into().map_err(|e| {
-                    error!("Failed to parse common message: {:?}", e);
-                    e
-                })?;
+            let mut frame: StdFrame = AnyMessage::Mining(message).try_into().map_err(|e| {
+                error!("Failed to parse common message: {:?}", e);
+                e
+            })?;
             let message_type = frame.get_header().unwrap().msg_type();
             let payload = frame.payload();
             let self_mutex = Arc::new(Mutex::new(self.clone()));
-            ParseMiningMessagesFromUpstream::handle_message_mining(self_mutex, message_type, payload)?;
+            let message = ParseMiningMessagesFromUpstream::handle_message_mining(
+                self_mutex,
+                message_type,
+                payload,
+            )?;
+
+            match message {
+                SendTo::Respond(message_for_upstream) => {
+                    todo!()
+                }
+                SendTo::None(Some(m)) => {
+                    self.sv1_server_sender.send(m).await;
+                }
+                _ => {}
+            }
         }
         Ok(())
     }
 
-    pub async fn create_channel(&mut self, downstream_id: u32, workername: String) -> Result<(), Error> {
-        let open_channel = Mining::OpenExtendedMiningChannel(OpenExtendedMiningChannel {
-            request_id: downstream_id,
-            user_identity: workername.try_into()?,
-            nominal_hash_rate: 1000.0, // TODO
-            max_target: u256_from_int(u64::MAX), // TODO
-            min_extranonce_size: 4, // TODO
-        });
-        self.upstream_sender.send(open_channel).await.map_err(|e| {
-            // TODO: Handle this error
-            error!("Failed to send open channel message to upstream: {:?}", e);
-            e
-        });
+    pub async fn create_channel(&self) -> Result<(), Error> {
+        while let Ok((downstream_id, workername)) = self.channel_opener_receiver.recv().await {
+            let open_channel = Mining::OpenExtendedMiningChannel(OpenExtendedMiningChannel {
+                request_id: downstream_id,
+                user_identity: workername.try_into()?,
+                nominal_hash_rate: 1000.0,           // TODO
+                max_target: u256_from_int(u64::MAX), // TODO
+                min_extranonce_size: 4,              // TODO
+            });
+            self.upstream_sender.send(open_channel).await.map_err(|e| {
+                // TODO: Handle this error
+                error!("Failed to send open channel message to upstream: {:?}", e);
+                e
+            });
+        }
         Ok(())
     }
-
 }
